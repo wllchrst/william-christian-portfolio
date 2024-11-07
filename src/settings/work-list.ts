29a97909f6d29327a8b996bf7b27b920b4d8ed53
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2024-11-07 17:53:28 +0700

    feat: Dummy data for work list

---
+++ src/settings/work-list.ts
@@ -0,0 +1,13 @@
+import { IWork } from "../interfaces/work-interface";
+
+const workList: IWork[] = [
+  {
+    title: "Travelio",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tortor justo, vestibulum eget viverra ac, tristique sit amet urna. Nunc eget hendrerit lacus. Nam faucibus felis vitae purus eleifend, id feugiat est mollis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed suscipit hendrerit dolor vel condimentum.",
+    mainPhoto: "",
+    secondPhoto: "",
+    thirdPhoto: "",
+    techStacks: [],
+  },
+];
