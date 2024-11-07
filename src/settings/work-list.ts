9bb69f05ff59db93ff645a9db73cab34c2cb3734
William Christian <william.christian001@binus.ac.id>
2024-11-07 22:35:52 +0700

    feat: dummy data for work

--- src/settings/work-list.ts
+++ src/settings/work-list.ts
@@ -2,12 +2,41 @@ import { IWork } from "../interfaces/work-interface";
 
 const workList: IWork[] = [
   {
-    title: "Travelio",
+    title: "Travelohi",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tortor justo, vestibulum eget viverra ac, tristique sit amet urna. Nunc eget hendrerit lacus. Nam faucibus felis vitae purus eleifend, id feugiat est mollis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed suscipit hendrerit dolor vel condimentum.",
-    mainPhoto: "",
-    secondPhoto: "",
-    thirdPhoto: "",
+    mainPhoto: "/public/project-images/travelohi/1.jpg",
+    secondPhoto: "/public/project-images/travelohi/2.jpg",
+    thirdPhoto: "/public/project-images/travelohi/3.jpg",
+    techStacks: [],
+  },
+  {
+    title: "Linkasa",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tortor justo, vestibulum eget viverra ac, tristique sit amet urna. Nunc eget hendrerit lacus. Nam faucibus felis vitae purus eleifend, id feugiat est mollis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed suscipit hendrerit dolor vel condimentum.",
+    mainPhoto: "/public/project-images/travelohi/1.jpg",
+    secondPhoto: "/public/project-images/travelohi/2.jpg",
+    thirdPhoto: "/public/project-images/travelohi/3.jpg",
+    techStacks: [],
+  },
+  {
+    title: "Linkasa",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tortor justo, vestibulum eget viverra ac, tristique sit amet urna. Nunc eget hendrerit lacus. Nam faucibus felis vitae purus eleifend, id feugiat est mollis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed suscipit hendrerit dolor vel condimentum.",
+    mainPhoto: "/public/project-images/travelohi/1.jpg",
+    secondPhoto: "/public/project-images/travelohi/2.jpg",
+    thirdPhoto: "/public/project-images/travelohi/3.jpg",
+    techStacks: [],
+  },
+  {
+    title: "Linkasa",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tortor justo, vestibulum eget viverra ac, tristique sit amet urna. Nunc eget hendrerit lacus. Nam faucibus felis vitae purus eleifend, id feugiat est mollis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed suscipit hendrerit dolor vel condimentum.",
+    mainPhoto: "/public/project-images/travelohi/1.jpg",
+    secondPhoto: "/public/project-images/travelohi/2.jpg",
+    thirdPhoto: "/public/project-images/travelohi/3.jpg",
     techStacks: [],
   },
 ];
+
+export default workList
