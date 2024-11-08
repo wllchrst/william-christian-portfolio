aaa12b2ef4a9513482ba03ea45d76fabac956141
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2024-11-08 13:57:26 +0700

    feat: Work Image Component

--- src/components/works/work-image-component.tsx
+++ src/components/works/work-image-component.tsx
@@ -1,18 +1,27 @@
 import { IWork } from "../../interfaces/work-interface";
 
 interface Props {
-    work: IWork;
+  work: IWork;
 }
 function WorkImageComponent({ work }: Props) {
-    return <div className="flex">
-        <div className="w-1/2 border border-pink-300">
-            <img src={work.mainPhoto} alt={work.mainPhoto} />
+  return (
+    <div className="flex gap-3 ">
+      <div className="w-2/3 rounded-md overflow-hidden shadow-lg relative hover:border hover:border-gray-400 transition-all">
+        <div className="absolute p-4 right-0 bg-blue-400 text-white rounded-sm transition-all hover:bg-blue-200 hover:text-black">
+          <p>Click Me!</p>
         </div>
-        <div className="flex w-1/2 border border-green-400">
-            <img src={work.secondPhoto} alt={work.secondPhoto} />
-            <img src={work.thirdPhoto} alt={work.thirdPhoto} />
+        <img src={work.mainPhoto} alt={work.mainPhoto} className="" />
+      </div>
+      <div className="flex w-1/3 flex-col justify-between gap-3 ">
+        <div className="rounded-md overflow-hidden shadow-lg hover:border hover:border-gray-400 transition-all">
+          <img src={work.secondPhoto} alt={work.secondPhoto} />
         </div>
+        <div className="rounded-md overflow-hidden shadow-lg hover:border hover:border-gray-400 transition-all">
+          <img src={work.thirdPhoto} alt={work.thirdPhoto} />
+        </div>
+      </div>
     </div>
+  );
 }
 
-export default WorkImageComponent
+export default WorkImageComponent;
