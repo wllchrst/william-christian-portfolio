fae901632a79aad084b72468f9c426faa8ded61c
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2024-11-08 13:57:10 +0700

    feat: styling for work list, layout

--- src/components/works/work-list-component.tsx
+++ src/components/works/work-list-component.tsx
@@ -2,12 +2,18 @@ import workList from "../../settings/work-list";
 import WorkCard from "./work-card";
 
 function WorkListComponent() {
-  return <>
-    <div className="border grid grid-cols-1 gap-4">
-      {workList.map((work, index) => (
-        <div key={index}><WorkCard work={work} /></div>
-      ))}
-    </div>
-  </>;
+  return (
+    <>
+      <div className="flex justify-center mt-14">
+        <div className="flex flex-col justify-center max-w-[900px] gap-8">
+          {workList.map((work, index) => (
+            <div key={index}>
+              <WorkCard work={work} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 export default WorkListComponent;
