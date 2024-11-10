3a8f0580329da70b2df3d5c3392c9f8b0c4b9131
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2024-11-10 18:16:30 +0700

    feat:  Hooks for controlling the works being shown

---
+++ src/hooks/use-work-controller.ts
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import { IWork } from "../interfaces/work-interface";
+import workList from "../settings/work-list";
+
+function useWorkController() {
+  const [works, setWorks] = useState<IWork[]>(workList);
+
+  return { works };
+}
+
+export default useWorkController;
