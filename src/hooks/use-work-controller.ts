af29d109d39706e4388b67d2239435876a291950
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2024-11-15 16:31:21 +0700

    fix: remove error when build

--- src/hooks/use-work-controller.ts
+++ src/hooks/use-work-controller.ts
@@ -5,7 +5,7 @@ import workList from "../settings/work-list";
 function useWorkController() {
   const [works, setWorks] = useState<IWork[]>(workList);
 
-  return { works };
+  return { works, setWorks };
 }
 
 export default useWorkController;
