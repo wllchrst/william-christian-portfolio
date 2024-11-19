d1db3c1b9d13b4922a74dbf10fb72478a50ca4e3
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2024-11-19 10:19:17 +0700

    fix: remove unused line

--- src/components/works/work-image-component.tsx
+++ src/components/works/work-image-component.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 function WorkImageComponent({ work }: Props) {
   return (
-    <div className="flex gap-3 ">
+    <div className="flex gap-3">
       <div className="w-2/3 rounded-md overflow-hidden shadow-lg relative hover:border-2 hover:border-gray-400 transition-all hover:cursor-hover">
         <div className="absolute p-4 right-0 bg-blue-400 text-white rounded-sm transition-all">
           <p>Click Me!</p>
