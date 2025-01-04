288a2385ceaa1849efe93387d5002f916df562c8
William Christian <william.christian001@binus.ac.id>
2025-01-04 09:05:02 +0700

    feat: remove custom cursor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      cursor:{
-        normal: 'url(/black-circle.svg), auto',
-        hover: 'url(/hover-circle.svg), auto'
-      }
+      cursor: {},
     },
   },
   plugins: [],
-}
+};
